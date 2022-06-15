1e9e528226af7c99542285f3028bc6ea10667503
Manu254rce <straussmoabit@gmail.com>
2022-06-15 21:49:19 +0300

    preparations for hosting node-modules on gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <BrowserRouter>
           <Routes>
-              <Route path='/' exact element={<Home/>}/>
+              <Route path='/' element={<Home/>}/>
               <Route path='/download' element={<DownloadPage />} />
           </Routes>
       </BrowserRouter>
